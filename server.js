fc26a828ee498849061568e4160f714a93c17adb
Aryan Ved <aryanved2002@gmail.com>
2022-07-29 18:53:34 -0400

    Revert "ready for dev mode"
    This reverts commit 888e50df8001f47c30c666dccc42836b028fdd34.

--- server.js
+++ server.js
@@ -128,5 +128,5 @@ app.post('/api/getRandom', (req, res) => {
 
 
 
-//app.listen(port, () => console.log(`Listening on port ${port}`)); //for the dev version
-app.listen(port, '129.97.25.211'); //for the deployed version, specify the IP address of the server
+app.listen(port, () => console.log(`Listening on port ${port}`)); //for the dev version
+//app.listen(port, '129.97.25.211'); //for the deployed version, specify the IP address of the server
