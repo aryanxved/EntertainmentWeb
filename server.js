d5aabd9be4bace87955a24545d384b5a38316f7b
Aryan Ved <aryanved2002@gmail.com>
2022-07-29 19:09:23 -0400

    update ip

--- server.js
+++ server.js
@@ -129,4 +129,4 @@ app.post('/api/getRandom', (req, res) => {
 
 
 //app.listen(port, () => console.log(`Listening on port ${port}`)); //for the dev version
-app.listen(port, '129.97.25.211'); //for the deployed version, specify the IP address of the server
+app.listen(port, '172.31.31.77'); //for the deployed version, specify the IP address of the server
